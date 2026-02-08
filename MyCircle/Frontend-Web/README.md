06b325e08e278b15ca7ad7781b9b41ff224d018e
Ishwar-Sanap <ishwarsanap512@gmail.com>
2026-02-08 22:15:46 +0530

    Setup VITE project and Tailwind for MyCircle frontend

---
+++ MyCircle/Frontend-Web/README.md
@@ -0,0 +1,5 @@
+# MyCircel Web-Frontend
+
+## Tools Used
+-   Crated appp using Vite bundle
+-   Used Tailwind CSS + daisy UI    
---
+++ MyCircle/Frontend-Web/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='text-3xl text-center bg-red-400 m-2'>MyCircle </h1>
+    <button class="btn btn-accent">Button</button>
+
+    </>
+  )
+}
+
+export default App
